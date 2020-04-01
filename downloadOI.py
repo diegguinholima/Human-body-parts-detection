a30533f5db871cc6d055821763a795de0104dee1
Diego Filipe Souza de Lim <dieggo.filipe@gmail.com>
2020-04-01 18:38:27 -0300

    bug fix in multiprocessing

--- downloadOI.py
+++ downloadOI.py
@@ -2,6 +2,8 @@
 
 #### Usage example: python3 downloadOI.py --classes 'Ice_cream,Cookie' --mode train
 
+### My usage example: python downloadOI_ori.py --classes "Human_eye" --mode validation
+
 import argparse
 import csv
 import subprocess
@@ -28,68 +30,71 @@ run_mode = args.mode
 
 threads = args.nthreads
 
-classes = []
-for class_name in args.classes.split(','):
-    classes.append(class_name)
+def run():
+    multiprocessing.freeze_support()
+    classes = []
+    for class_name in args.classes.split(','):
+        classes.append(class_name)
 
-with open('./class-descriptions-boxable.csv', mode='r') as infile:
-    reader = csv.reader(infile)
-    dict_list = {rows[1]:rows[0] for rows in reader}
+    with open('./class-descriptions-boxable.csv', mode='r') as infile:
+        reader = csv.reader(infile)
+        dict_list = {rows[1]:rows[0] for rows in reader}
 
-subprocess.run(['rm', '-rf', run_mode])
-subprocess.run([ 'mkdir', run_mode])
+    subprocess.run(['rm', '-rf', run_mode])
+    subprocess.run([ 'mkdir', run_mode])
 
-pool = thread_pool(threads)
-commands = []
-cnt = 0
+    pool = thread_pool(threads)
+    commands = []
+    cnt = 0
 
-for ind in range(0, len(classes)):
-    
-    class_name = classes[ind]
-    print("Class "+str(ind) + " : " + class_name)
-    
-    subprocess.run([ 'mkdir', run_mode+'/'+class_name])
-
-    command = "grep "+dict_list[class_name.replace('_', ' ')] + " ./" + run_mode + "-annotations-bbox.csv"
-    class_annotations = subprocess.run(command.split(), stdout=subprocess.PIPE).stdout.decode('utf-8')
-    class_annotations = class_annotations.splitlines()
-
-    for line in class_annotations:
-
-        line_parts = line.split(',')
+    for ind in range(0, len(classes)):
         
-        #IsOccluded,IsTruncated,IsGroupOf,IsDepiction,IsInside
-        if (args.occluded==0 and int(line_parts[8])>0):
-            print("Skipped %s",line_parts[0])
-            continue
-        if (args.truncated==0 and int(line_parts[9])>0):
-            print("Skipped %s",line_parts[0])
-            continue
-        if (args.groupOf==0 and int(line_parts[10])>0):
-            print("Skipped %s",line_parts[0])
-            continue
-        if (args.depiction==0 and int(line_parts[11])>0):
-            print("Skipped %s",line_parts[0])
-            continue
-        if (args.inside==0 and int(line_parts[12])>0):
-            print("Skipped %s",line_parts[0])
-            continue
-
-        cnt = cnt + 1
-
-        command = 'aws s3 --no-sign-request --only-show-errors cp s3://open-images-dataset/'+run_mode+'/'+line_parts[0]+'.jpg '+ run_mode+'/'+class_name+'/'+line_parts[0]+'.jpg'
-        commands.append(command)
+        class_name = classes[ind]
+        print("Class "+str(ind) + " : " + class_name)
         
-        with open('%s/%s/%s.txt'%(run_mode,class_name,line_parts[0]),'a') as f:
-            f.write(','.join([class_name, line_parts[4], line_parts[5], line_parts[6], line_parts[7]])+'\n')
+        subprocess.run([ 'mkdir', run_mode+'/'+class_name])
 
-print("Annotation Count : "+str(cnt))
-commands = list(set(commands))
-print("Number of images to be downloaded : "+str(len(commands)))
+        command = "grep "+dict_list[class_name.replace('_', ' ')] + " ./" + run_mode + "-annotations-bbox.csv"
+        class_annotations = subprocess.run(command.split(), stdout=subprocess.PIPE).stdout.decode('utf-8')
+        class_annotations = class_annotations.splitlines()
 
-list(tqdm(pool.imap(os.system, commands), total = len(commands) ))
+        for line in class_annotations:
 
-pool.close()
-pool.join()
+            line_parts = line.split(',')
+            
+            #IsOccluded,IsTruncated,IsGroupOf,IsDepiction,IsInside
+            if (args.occluded==0 and int(line_parts[8])>0):
+                print("Skipped %s",line_parts[0])
+                continue
+            if (args.truncated==0 and int(line_parts[9])>0):
+                print("Skipped %s",line_parts[0])
+                continue
+            if (args.groupOf==0 and int(line_parts[10])>0):
+                print("Skipped %s",line_parts[0])
+                continue
+            if (args.depiction==0 and int(line_parts[11])>0):
+                print("Skipped %s",line_parts[0])
+                continue
+            if (args.inside==0 and int(line_parts[12])>0):
+                print("Skipped %s",line_parts[0])
+                continue
 
+            cnt = cnt + 1
 
+            command = 'aws s3 --no-sign-request --only-show-errors cp s3://open-images-dataset/'+run_mode+'/'+line_parts[0]+'.jpg '+ run_mode+'/'+class_name+'/'+line_parts[0]+'.jpg'
+            commands.append(command)
+            
+            with open('%s/%s/%s.txt'%(run_mode,class_name,line_parts[0]),'a') as f:
+                f.write(','.join([class_name, line_parts[4], line_parts[5], line_parts[6], line_parts[7]])+'\n')
+
+    print("Annotation Count : "+str(cnt))
+    commands = list(set(commands))
+    print("Number of images to be downloaded : "+str(len(commands)))
+
+    list(tqdm(pool.imap(os.system, commands), total = len(commands) ))
+
+    pool.close()
+    pool.join()
+
+if __name__ == '__main__':
+    run()
